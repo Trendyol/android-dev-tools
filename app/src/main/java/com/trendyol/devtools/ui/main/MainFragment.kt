c9c175b4472664a7d59c96d7219974c82d0ef77a
Mert <mertnevzatyuksel@gmail.com>
2021-05-10 14:28:54 +0300

    Initial commit

---
+++ app/src/main/java/com/trendyol/devtools/ui/main/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.trendyol.devtools.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.trendyol.devtools.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/trendyol/devtools/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.trendyol.devtools.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
