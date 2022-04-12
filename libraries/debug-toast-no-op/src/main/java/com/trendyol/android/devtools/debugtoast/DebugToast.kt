be1caf1cae88609ff62cc84750248a851fdc76a5
İbrahim Süren <ibrahimsn98@gmail.com>
2022-04-12 14:31:00 +0300

    Implement debug-toast-no-op

---
+++ libraries/debug-toast-no-op/src/main/java/com/trendyol/android/devtools/debugtoast/DebugToast.kt
@@ -0,0 +1,17 @@
+package com.trendyol.android.devtools.debugtoast
+
+import android.content.Context
+
+class DebugToast private constructor() {
+
+    companion object {
+
+        fun init(context: Context) {
+            // no-op
+        }
+
+        fun show(context: Context, text: String, duration: Int) {
+            // no-op
+        }
+    }
+}
