c4c442751ef4dbf1b978891c83b71217b06f4240
mert.yuksel <mert.yuksel@trendyol.com>
2025-07-03 11:47:12 +0200

    Update gradle version to 8.13 and create version catalogs

---
+++ libraries/http-inspector/build.gradle.kts
@@ -0,0 +1,91 @@
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.ktlint.gradle)
+    `maven-publish`
+}
+
+android {
+    namespace = "com.trendyol.android.devtools.httpinspector"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        // aarMetadata.minCompileSdk = 34
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    publishing {
+        singleVariant("release")
+    }
+}
+
+extra.apply {
+    set("PUBLISH_GROUP_ID", "com.trendyol.android.devtools")
+    set("PUBLISH_VERSION", "0.2.0")
+    set("PUBLISH_ARTIFACT_ID", "http-inspector")
+    set("PUBLISH_DESCRIPTION", "Android HTTP Inspector")
+    set("PUBLISH_URL", "https://github.com/Trendyol/android-dev-tools")
+    set("PUBLISH_LICENSE_NAME", "Android DevTools License")
+    set("PUBLISH_LICENSE_URL", "https://github.com/Trendyol/android-dev-tools/blob/master/LICENSE")
+    set("PUBLISH_SCM_CONNECTION", "scm:git:github.com/Trendyol/android-dev-tools.git")
+    set("PUBLISH_SCM_DEV_CONNECTION", "scm:git:ssh://github.com/Trendyol/android-dev-tools.git")
+    set("PUBLISH_SCM_URL", "https://github.com/Trendyol/android-dev-tools/tree/main")
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.android.material)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.startup.runtime)
+
+    // Room
+    implementation(libs.bundles.room)
+    ksp(libs.androidx.room.compiler)
+
+    // Ktor
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.ktor.server.websockets)
+    implementation(libs.ktor.gson)
+
+    // HTTP
+    implementation(libs.squareup.okhttp)
+    implementation(libs.google.gson)
+    implementation(libs.squareup.moshi.kotlin)
+
+    // RxJava
+    implementation(libs.rxjava)
+    implementation(libs.rxandroid)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+apply(from = "${rootProject.rootDir}/scripts/publish-module.gradle")
