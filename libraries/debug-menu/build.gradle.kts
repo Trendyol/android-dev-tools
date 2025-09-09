706479d24dc7a924d579acaa4c073c8b68022310
goksu.turker <goksu.turker@trendyol.com>
2025-09-09 08:56:43 +0300

    ANDDEV-8995 | Add koin to debug-menu module

--- libraries/debug-menu/build.gradle.kts
+++ libraries/debug-menu/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 group = "com.trendyol.android.devtools"
-version = "0.5.1"
+version = "0.6.0"
 
 publishConfig {
     defaultConfiguration(
@@ -54,4 +54,6 @@ dependencies {
     implementation(libs.google.android.material)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.embeddedKoinCore)
+    implementation(libs.embeddedKoinAndroid)
 }
--- libraries/debug-menu/src/main/java/com/trendyol/android/devtools/debugmenu/internal/ui/DebugMenuFragment.kt
+++ libraries/debug-menu/src/main/java/com/trendyol/android/devtools/debugmenu/internal/ui/DebugMenuFragment.kt
@@ -4,20 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.trendyol.android.devtools.debugmenu.DebugActionItem
 import com.trendyol.android.devtools.debugmenu.R
 import com.trendyol.android.devtools.debugmenu.databinding.DebugMenuFragmentBinding
-import com.trendyol.android.devtools.debugmenu.internal.di.ContextContainer
+import com.trendyol.android.devtools.debugmenu.internal.di.DebugMenuKoinComponent
 import com.trendyol.android.devtools.debugmenu.internal.ext.viewBinding
 import com.trendyol.android.devtools.debugmenu.internal.ui.DebugMenuActivity.Companion.KEY_TITLE
+import embedded.koin.androidx.viewmodel.ext.android.viewModel
 
-internal class DebugMenuFragment : Fragment(R.layout.debug_menu_fragment) {
+internal class DebugMenuFragment : Fragment(R.layout.debug_menu_fragment), DebugMenuKoinComponent {
 
     private val binding: DebugMenuFragmentBinding by viewBinding(DebugMenuFragmentBinding::bind)
-    private val viewModel by viewModels<DebugMenuViewModel> {
-        ContextContainer.debugMenuContainer.DebugMenuViewModelFactory()
-    }
+    private val viewModel: DebugMenuViewModel by viewModel()
     private val adapter: DebugMenuAdapter = DebugMenuAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
