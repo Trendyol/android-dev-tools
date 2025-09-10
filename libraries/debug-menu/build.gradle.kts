aa7059a2d267fb27af55203fac92b8e53dfc2119
goksu.turker <goksu.turker@trendyol.com>
2025-09-10 12:19:02 +0300

    ANDDEV-8995 | Fix debug menu toolbar overlap issue in API 35

--- libraries/debug-menu/build.gradle.kts
+++ libraries/debug-menu/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 java.toolchain.languageVersion = JavaLanguageVersion.of(libs.versions.java.get().toString().toInt())
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     namespace = "com.trendyol.android.devtools.debugmenu"
 
     defaultConfig {
